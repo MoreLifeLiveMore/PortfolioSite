bb03de72a9e769077d50d788a563d0485e010c9f
MoreLifeLiveMore <aadon.miller@gmail.com>
2024-09-16 16:14:33 -0400

    need to fix nav hover

--- typingAnimation.js
+++ typingAnimation.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var div = document.querySelector('.content .typewriter-text');
-    var newText = 'playing football.';
-  
-    setTimeout(function () {
-      div.setAttribute('data-text', newText);
-      div.style.animation = 'typing 5s steps(40) forwards';
-    }, 7000); // Adjust the timing according to your need
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var div = document.querySelector(".typewriter");
+  var newText = "playing football.";
+
+  setTimeout(function () {
+    div.setAttribute("data-text", newText);
+    div.animation = "typing 5s steps(40) forwards";
+  }, 1000); // Adjust the timing according to your need
+});
