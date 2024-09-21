b0a0c76af5db748e35c8096f3ebf41c8ddad4207
MoreLifeLiveMore <aadon.miller@gmail.com>
2024-09-21 18:34:09 -0400

    index Page Complete

---
+++ autoType.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typewriter",{
+    strings : ["Software Engineer","Software Developer","Game Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false
+});
--- typingAnimation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var div = document.querySelector(".typewriter");
-  var newText = "playing football.";
-
-  setTimeout(function () {
-    div.setAttribute("data-text", newText);
-    div.animation = "typing 5s steps(40) forwards";
-  }, 1000); // Adjust the timing according to your need
-});
